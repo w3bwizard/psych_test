51667596e18acc5c91a5d97e354f1ff03dff06b5
W3bwizard <w3bwiz4rd@gmail.com>
2021-11-28 15:22:46 +0200

    update grid markup

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+var elem = document.documentElement;
+
+/* Просмотр в полноэкранном режиме */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Закрыть полный экран */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+
+window.addEventListener("load",function() {
+  openFullscreen()
+});
